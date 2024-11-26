21ada43aa1179cdf413374aca657639b5bcc044f
star-dust <beyond-intelligence@gmail.com>
2024-11-26 21:39:47 +0300

    new project

---
+++ peges/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'peges/index.html',{'name':'ahmed','age':'6'})
+
+def about (request):
+    pass
+
+def base (request):
+    return render(request,'templates/base.html' )
